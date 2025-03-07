9805fc213ce3b411a39e9855630384135962378d
Anil Kumar <114756436+anil-52026@users.noreply.github.com>
2025-03-07 23:48:25 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -30,8 +30,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    // origin: process.env.CLIENT_BASE_URL,
-    origin: "*",
+    origin: process.env.CLIENT_BASE_URL,
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
